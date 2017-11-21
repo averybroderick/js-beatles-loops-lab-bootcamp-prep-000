ba8c0b075fa12e3eadf3aae5f280d0ac024f795f
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-21 18:42:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function theBeatlesPlay(musicians, instruments) {
+  var myArr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    myArr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return myArr;
+}
+
+function johnLennonFacts(facts) {
+  while (facts.length > 0) {
+    facts[i] + "!!!";
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var myArr = [];
+  do {
+    myArr.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+}
